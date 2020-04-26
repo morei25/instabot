99b7f2d934da72ec80c922090fc4db72cc7ef164
morei <moreira-marques.daniel@guj.de>
2020-04-26 18:04:48 +0200

    Added Headless Window

--- auto.py
+++ auto.py
@@ -1,7 +1,7 @@
 from instapy import InstaPy
 
-session = InstaPy(username="danielmoreira.dm49@gmail.com", password="DerBot25").login()
+session = InstaPy(username="danielmoreira.dm49@gmail.com", password="DerBot25", headless_browser=True).login()
 session.like_by_tags(["bmw","mercedes"], amount=5)
-seesion.set_dont_like(["porshe", "naked"])
+session.set_dont_like(["porshe", "naked"])
 session.set_do_follow(True, percentage=45)
 
