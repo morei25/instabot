164d23fdba50d976da461303c49f2cb0a84bd7a5
morei <moreira-marques.daniel@guj.de>
2020-04-26 17:56:27 +0200

    Like some Images

--- auto.py
+++ auto.py
@@ -1,3 +1,6 @@
 from instapy import InstaPy
 
-InstaPy(username="danielmoreira.dm49@gmail.com", password="DerBot25").login()
+session = InstaPy(username="danielmoreira.dm49@gmail.com", password="DerBot25").login()
+session.like_by_tags(["bmw","mercedes"], amount=5)
+seesion.set_dont_like(["porshe", "naked"])
+
