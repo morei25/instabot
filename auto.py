23e67384a3655a7b52d4894e196836619688237c
morei <moreira-marques.daniel@guj.de>
2020-04-26 17:58:46 +0200

    Follow Persons

--- auto.py
+++ auto.py
@@ -3,4 +3,5 @@ from instapy import InstaPy
 session = InstaPy(username="danielmoreira.dm49@gmail.com", password="DerBot25").login()
 session.like_by_tags(["bmw","mercedes"], amount=5)
 seesion.set_dont_like(["porshe", "naked"])
+session.set_do_follow(True, percentage=45)
 
